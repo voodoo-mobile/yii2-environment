e388f53af09f375ee1855fb476188211598fc0f8
Alexander Kryshtalev <alex@voodoo.rocks>
2019-03-21 20:53:43 +0200

    Change Object to BaseObject as a parent class of Flavor

--- src/Flavor.php
+++ src/Flavor.php
@@ -3,7 +3,7 @@
 namespace vr\environment;
 
 use Closure;
-use yii\base\Object;
+use yii\base\BaseObject;
 use yii\helpers\ArrayHelper;
 
 /**
@@ -11,7 +11,7 @@ use yii\helpers\ArrayHelper;
  * @package vr\environment
  * @property bool $isActive
  */
-abstract class Flavor extends Object
+abstract class Flavor extends BaseObject
 {
     /**
      * @var string
